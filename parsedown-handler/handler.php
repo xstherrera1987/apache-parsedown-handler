7567e55d00ca07192116d70368bf5e8d28aa0703
Joe Herrera <xstherrera1987@gmail.com>
2015-04-29 00:40:55 +0100

    add submodules, configure handler

--- parsedown-handler/handler.php
+++ parsedown-handler/handler.php
@@ -2,20 +2,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="/parsedown-handler/style.css">
-	<meta name="content-type" http-equiv="content-type" value="text/html; utf-8">
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<link rel="stylesheet" type="text/css" href="/parsedown-handler/gfm-css/github-markdown.css">
 </head>
 <body>
 <?php
-
-	// require('markdown.php');
+	require('parsedown-handler/parsedown/Parsedown.php');
 	$legalExtensions = array('md', 'markdown');
 	$file = realpath($_SERVER['PATH_TRANSLATED']);
 
 	if($file
 		&& in_array(strtolower(substr($file,strrpos($file,'.')+1)), $legalExtensions)
 		&& substr($file,0,strlen($_SERVER['DOCUMENT_ROOT'])) == $_SERVER['DOCUMENT_ROOT']) {
-		echo Markdown(file_get_contents($file));
+
+		$Parsedown = new Parsedown();
+		echo $Parsedown->text( file_get_contents($file) );
+
 	} else { echo "<p>Bad filename given</p>"; }
 ?>
 </body>
